edf4c952b047e2199679b8b8ee078622c0ca7b6e
Denis Kosolapov <denis@kosolapov.info>
2024-12-23 10:45:42 +0300

    Initial commit

---
+++ meteorologBackend/src/test/kotlin/tema/meteorolog/MeteorologApplicationTests.kt
@@ -0,0 +1,13 @@
+package tema.meteorolog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MeteorologApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
